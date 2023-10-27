6bd73055dec3d5d5cde7f71bc18741a35d72d24a
mscha <m.saleh.sedeghi@gmail.com>
2023-10-27 20:44:24 +0330

    Initial commit

---
+++ app/src/main/java/com/mscha/routine/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mscha.routine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
